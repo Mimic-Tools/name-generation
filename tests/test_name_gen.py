be293e696bad28cfec960e9143dca688a6d45fe0
Ian Hunter <ianfhunter@gmail.com>
2020-10-29 11:24:34 +0000

    Create test_name_gen.py

---
+++ tests/test_name_gen.py
@@ -0,0 +1,17 @@
+import pytest
+from ..generate import generate, Name
+
+class Mock():
+    def __init__(self):
+        pass
+
+def test_generate_name(
+    a = Mock()
+    a.gender_male = True
+    a.gender_female = True
+    a.gender_neutral = True
+    a.order = Name.NameOrder.Western
+    a.origin = Name.Origin.Mountain
+    a.namebank = Name.NameBank.Dwarf
+    a.has_position = True
+    generate(a)
