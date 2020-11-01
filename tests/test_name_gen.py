353769cf924c2671c97ad5bea586ebd97487468e
ianfhunter <ianfhunter@gmail.com>
2020-11-01 14:34:36 +0000

    - Add collapsable segments to HTML
    - Change button for link (Ajax Requests for POSTING)
    - Verbosity Flag for python
    - StringToEnum function for better handling of external handlers
    - Improved tests

--- tests/test_name_gen.py
+++ tests/test_name_gen.py
@@ -2,31 +2,34 @@ import sys
 import os
 fp = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(1, os.path.join(fp, '../src/'))
-from name_generation.generate import generate, Name
+from name_generation.generate import generate, Name, StringToEnum
 
 class Mock():
     def __init__(self):
         pass
 
+def initialize_mock():
+    m = Mock()
+    m.gender_male = False
+    m.gender_female = False
+    m.gender_neutral = True
+    m.order = Name.NameOrder.Western
+    m.origin = Name.Origin.Mountain
+    m.namebank = Name.NameBank.Dwarf
+    m.has_position = True
+    m.verbose = False
+    return m
 
 def test_generate_neutral_dwarf_name():
-    a = Mock()
-    a.gender_male = False
-    a.gender_female = False
-    a.gender_neutral = True
-    a.order = Name.NameOrder.Western
-    a.origin = Name.Origin.Mountain
-    a.namebank = Name.NameBank.Dwarf
-    a.has_position = True
-    generate(a)
+    a = initialize_mock()
+    name = generate(a)
+    assert(len(name) > 0)
 
 def test_generate_female_dwarf_name():
-    a = Mock()
-    a.gender_male = False
+    a = initialize_mock()
     a.gender_female = True
-    a.gender_neutral = True
-    a.order = Name.NameOrder.Western
-    a.origin = Name.Origin.Mountain
-    a.namebank = Name.NameBank.Dwarf
-    a.has_position = True
-    generate(a)
+    name = generate(a)
+    assert(len(name) > 0)
+
+def test_string2enum():
+    assert(StringToEnum("Dwarf") == Name.NameBank.Dwarf)
