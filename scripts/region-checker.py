1cf8e07cfe388eabc711bfa0a5895afd267c86c1
Ian Hunter <ianfhunter@gmail.com>
2020-11-02 14:15:57 +0000

    Update region-checker.py

--- scripts/region-checker.py
+++ scripts/region-checker.py
@@ -29,16 +29,16 @@ def find_availability(search_folder, rows):
 
 def find_gender_availability(search_folder, rows):
     report_dictionary = {}
-    for r in rows:
-        for gender in ["male", "female", "neutral"]:
+    for items in rows:
+        report_dictionary[items] = {}
+        print(f"Checking for {items} files")
+        # for item in search_folder:
+        for gender in ["male", "female", "neutral"]:          
             if gender in ["male", "female"]:
                 filename = r.split(".txt")[0] + f"-{gender}.txt"
             else:
                 filename = r
-            report_dictionary[gender] = {}
-            print(f"Checking for {filename} file")
-            for item in search_folder:
-                report_dictionary[gender][item] = isfile(join(join(prefix, item), filename))
+            report_dictionary[items][filename] = isfile(join(join(prefix, filename), items))
     return report_dictionary
 
 def generate_reports(entries, report_headers, title="Region", name="region_report"):
